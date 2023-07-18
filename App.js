b0804a1921977e1caac563a08966510995cfbb10
arttu <arttutjpesonen@gmail.com>
2023-07-18 12:07:34 +0300

    exercises 10.1-10.2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Small changes...</Text>
+      <Text> My app seems to be working in Android Studio! </Text>
       <StatusBar style="auto" />
     </View>
   );
