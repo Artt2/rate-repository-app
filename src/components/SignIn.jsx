431f8427f602369a2c823f99a07a29229e1f4eed
arttu <arttutjpesonen@gmail.com>
2023-07-19 13:34:12 +0300

    exercise 10.9

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,10 +1,20 @@
 import { View, Pressable, StyleSheet } from 'react-native';
-import { Formik, useField } from 'formik';
+import { Formik } from 'formik';
+import * as yup from "yup";
 
 import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 
 import theme from '../theme';
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required("Username is required"),
+  password: yup
+    .string()
+    .required("Password is required")
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -12,8 +22,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     paddingHorizontal: 10
   },
-  textInput: {
-    color: theme.colors.textSecondary,
+  textInput: {  //this styling could be done in FormikTextInput as well
+    color: theme.colors.textPrimary,
     borderWidth: 1,
     borderColor: theme.colors.textSecondary,
     borderRadius: 5,
@@ -30,7 +40,6 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     marginVertical: 10,
-    //alignSelf: "center",
   }
 });
 
@@ -40,19 +49,30 @@ const initialValues = {
 };
 
 const SignIn = () => {
-  const onSubmit = (values) => {
+  const handleSignIn = (values) => {
     console.log(values);
   };
 
   return (
-    <Formik initialValues={initialValues}>
-      <View style={styles.container}>
-        <FormikTextInput style={styles.textInput} name="username" placeholder="Username" />
-        <FormikTextInput style={styles.textInput} name="password" placeholder="Password" />
-        <Pressable style={styles.button} onPress={onSubmit}>
-          <Text style={styles.buttonText}>Sign in</Text>
-        </Pressable>
-      </View>
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSignIn}>
+      {({ handleSubmit }) => (
+        <View style={styles.container}>
+          <FormikTextInput 
+            style={styles.textInput} 
+            name="username" 
+            placeholder="Username" 
+          />
+          <FormikTextInput 
+            style={styles.textInput} 
+            name="password" 
+            placeholder="Password"
+            secureTextEntry={true}
+          />
+          <Pressable style={styles.button} onPress={handleSubmit}>
+            <Text style={styles.buttonText}>Sign in</Text>
+          </Pressable>
+        </View>
+      )}
     </Formik>
   );
 };
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,12 +1,32 @@
 /* eslint-disable no-unused-vars */
 import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+import theme from '../theme';
+
+/*
+  Currently only errors change the style.
+  The styling of FormikTextInput could be done completely here,
+  making each input identical (unless more style is added.)
+*/
+const styles = StyleSheet.create({
+  errorBorder: {
+    borderColor: "#d73a4a",
+    borderWidth: 1
+  },
+});
 
 const TextInput = ({ style, error, ...props }) => {
   const textInputStyle = [style];
 
-  return <NativeTextInput style={textInputStyle} {...props} />;
+  /*
+    If there are validation errors, error is set to true by FormTextInput.
+    Then more styling is added, a red border for the NativeTextInput.
+  */
+  if (error) {
+    textInputStyle.push(styles.errorBorder);
+  }
+
+  return <NativeTextInput placeholderTextColor={theme.colors.textSecondary} style={textInputStyle} {...props} />;
 };
 
 export default TextInput;
